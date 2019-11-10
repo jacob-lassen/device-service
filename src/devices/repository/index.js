4b5755ac94db508fa0031f70a18b43127eea6346
jacob <jabberla5@gmail.com>
2019-11-10 10:32:16 +0100

    Device repository

--- src/devices/repository/index.js
+++ src/devices/repository/index.js
@@ -1,29 +1,59 @@
-function makeDeviceDb({ dbConnection }) {
+const makeDevice = require('../entity');
+
+function makeDeviceRepository({ dbConnection }) {
     return Object.freeze({
         findAll,
-        findById,
-        insert,
+        findByUuid,
+        create,
         remove,
         update,
     })
 
-    async function findAll({}) {
-
+    async function findAll() {
+        const rows = await dbConnection('devices')
+            .select()
+        return rows.map(makeDevice);
     }
 
-    async function findById({}) {
-
+    async function findByUuid(uuid) {
+        const [row] = await dbConnection('devices')
+        .where({ uuid: uuid });
+        return makeDevice(row);
     }
 
-    async function insert({}) {
-
+    async function create(device) {
+        const insert = {
+            uuid: device.getUuid(),
+            ip: device.getIp(),
+            mac: device.getMac(),
+            name: device.getName(),
+            lastSeen: device.getLastSeen(),
+            createdAt: device.getCreatedAt(),
+            updatedAt: device.getUpdatedAt(),
+        }
+        await dbConnection('devices').insert(insert);
+        return null;
     }
 
-    async function remove({}) {
-
+    async function remove(uuid) {
+        await dbConnection('devices')
+            .where({ uuid: uuid })
+            .del()
     }
 
-    async function update({}) {
-        
+    async function update(device) {
+        const updates = {
+            name: device.getName(),
+            ip: device.getIp(),
+            mac: device.getMac(),
+            lastSeen: device.getLastSeen(),
+            updatedAt: device.getUpdatedAt(),
+        };
+        await dbConnection('devices')
+            .where({ uuid: device.getUuid() })
+            .update(updates);
+        return null;
     }
 }
+
+module.exports = makeDeviceRepository;
