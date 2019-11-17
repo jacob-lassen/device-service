7974891d95c39533a499c24c23e573880cf6ea67
jacob <jabberla5@gmail.com>
2019-11-17 14:00:06 +0100

    makeConnection is now a named function

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,11 +1,15 @@
 const knex = require('knex');
 
-module.exports = knex({
-    client: 'pg',
-    connection: 'postgres://master:password@localhost:5432/home',
-    searchPath: ['device'],
-    pool: { 
-        min: 0,
-        max: 4
-    }
-});
+function makeDbConnection() {
+    return knex({
+        client: 'pg',
+        connection: 'postgres://master:password@localhost:5432/home',
+        searchPath: ['device'],
+        pool: { 
+            min: 0,
+            max: 4
+        }
+    });
+}
+
+module.exports = makeDbConnection;
